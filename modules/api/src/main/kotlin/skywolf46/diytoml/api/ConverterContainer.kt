9079864b1f90cd48d8bfc7dc733127359022494d
skywolf46 <skywolf461@gmail.com>
2023-05-28 06:34:47 +0900

    feat(api): add ConverterContainer#isConvertible

--- modules/api/src/main/kotlin/skywolf46/diytoml/api/ConverterContainer.kt
+++ modules/api/src/main/kotlin/skywolf46/diytoml/api/ConverterContainer.kt
@@ -1,7 +1,10 @@
 package skywolf46.diytoml.api
 
 import arrow.core.Either
+import kotlin.reflect.KClass
 
 interface ConverterContainer {
     fun <T : Any> convert(any: T): Either<Throwable, Any>
+
+    fun <T : Any> isConvertible(target: KClass<T>): Boolean
 }
