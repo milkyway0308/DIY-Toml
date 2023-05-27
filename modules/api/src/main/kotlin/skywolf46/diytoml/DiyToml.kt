f2777bce5af562c510b6f9fc7e0f19d93d5063b8
skywolf46 <skywolf461@gmail.com>
2023-05-28 01:33:22 +0900

    feat(impl): remove unnecessary imports

--- modules/api/src/main/kotlin/skywolf46/diytoml/DiyToml.kt
+++ modules/api/src/main/kotlin/skywolf46/diytoml/DiyToml.kt
@@ -1,9 +1,6 @@
 package skywolf46.diytoml
 
-import skywolf46.diytoml.api.TomlProvider
-import skywolf46.diytoml.api.TomlReader
-import skywolf46.diytoml.api.TomlSpec
-import skywolf46.diytoml.api.TomlWriter
+import skywolf46.diytoml.api.*
 import skywolf46.diytoml.api.annotations.InternalAPI
 
 @OptIn(InternalAPI::class)
