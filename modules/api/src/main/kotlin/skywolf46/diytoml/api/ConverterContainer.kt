a397dca0f49e1201516401f5e9fd461b37e0c203
skywolf46 <skywolf461@gmail.com>
2023-05-28 08:18:46 +0900

    feat(api): add consequential converter and registration to ConverterContainer

--- modules/api/src/main/kotlin/skywolf46/diytoml/api/ConverterContainer.kt
+++ modules/api/src/main/kotlin/skywolf46/diytoml/api/ConverterContainer.kt
@@ -6,5 +6,11 @@ import kotlin.reflect.KClass
 interface ConverterContainer {
     fun <T : Any> convert(any: T): Either<Throwable, Any>
 
+    fun <FROM: Any, TO : Any> convertTo(target: KClass<TO>, any: FROM): Either<Throwable, TO>
+
     fun <T : Any> isConvertible(target: KClass<T>): Boolean
+
+    fun <FROM : Any> registerConsequentialConverter(from: KClass<FROM>, converter: Converter<FROM, Any>)
+
+    fun <FROM : Any, TO : Any> registerConverter(from: KClass<FROM>, to: KClass<TO>, converter: Converter<FROM, TO>)
 }
