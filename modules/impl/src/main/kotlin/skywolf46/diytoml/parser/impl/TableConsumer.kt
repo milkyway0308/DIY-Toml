78936c1f331f20b0bbdc6559de1fcf592c52b712
skywolf46 <skywolf461@gmail.com>
2023-06-12 09:20:42 +0900

    fix(impl): fix TableConsumer consumes single table only

--- modules/impl/src/main/kotlin/skywolf46/diytoml/parser/impl/TableConsumer.kt
+++ modules/impl/src/main/kotlin/skywolf46/diytoml/parser/impl/TableConsumer.kt
@@ -1,24 +1,40 @@
 package skywolf46.diytoml.parser.impl
 
 import arrow.core.Either
-import arrow.core.EitherNel
 import arrow.core.getOrElse
 import arrow.core.left
+import arrow.core.right
 import skywolf46.diytoml.TomlElement
 import skywolf46.diytoml.parser.ContextConsumer
 import skywolf46.diytoml.parser.TomlContext
-import skywolf46.diytoml.util.collect
 
-class TableConsumer : ContextConsumer<EitherNel<TomlElement.Table<*>, TomlElement.Table<*>>>() {
+class TableConsumer : ContextConsumer<TomlElement.ConstructedTable<*>>() {
 
-
-    override fun consume(tomlContext: TomlContext): Either<Throwable, EitherNel<TomlElement.Table<*>, TomlElement.Table<*>>> {
+    override fun consume(tomlContext: TomlContext): Either<Throwable, TomlElement.ConstructedTable<*>> {
         val info = TableInfoConsumer().consume(tomlContext).getOrElse { return it.left() }
-        val data = RowConsumer().consume(tomlContext).getOrElse { throw it }
-        println("Table info: $info")
-        println("Key name: ${data.first} ")
-        println("Value: ${data.second}(${data.second.javaClass.name} ")
-        return Exception().left()
+        val dataList = mutableMapOf<String, TomlElement<*>>()
+        while (tomlContext.current().isSome()) {
+            val current = tomlContext.current().getOrElse { return Exception("Impossible operation occurred").left() }
+            current.mark()
+            if (current.consume() == '[') {
+                current.reset()
+                break
+            }
+            current.reset()
+            val data = RowConsumer().consume(tomlContext)
+            if (data.isLeft()) {
+                break
+            }
+            data.onRight {
+                val name = "${info.tableName}.${it.rowName}"
+                if (name in dataList) {
+                    return Exception().left()
+                }
+                dataList[name] = it.rowValue
+            }
+        }
+
+        return TomlElement.ConstructedTable(info, dataList).right()
     }
 
 
